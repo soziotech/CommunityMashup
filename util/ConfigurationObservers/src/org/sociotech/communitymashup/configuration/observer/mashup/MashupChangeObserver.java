55d6adf9961859b52e30345ce8b80ef5fbed4dc2
Peter Lachenmaier <peter.lachenmaier@kooperationssysteme.de>
2013-02-12 11:26:00 +0100

    - new observer for changes to mashup configurations

---
+++ util/ConfigurationObservers/src/org/sociotech/communitymashup/configuration/observer/mashup/MashupChangeObserver.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Peter Lachenmaier - Cooperation Systems Center Munich (CSCM).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Peter Lachenmaier - Design and initial implementation
+ ******************************************************************************/
+package org.sociotech.communitymashup.configuration.observer.mashup;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.util.EContentAdapter;
+import org.sociotech.communitymashup.application.Mashup;
+import org.sociotech.communitymashup.application.util.ApplicationAdapterFactory;
+
+/**
+ * Observer for changes to mashup container configurations.
+ * 
+ * @author Peter Lachenmaier
+ */
+public class MashupChangeObserver extends ApplicationAdapterFactory {
+	
+	/**
+	 * The observer of the mashup container.
+	 */
+	private MashupChangedInterface observerToNotify;
+	
+	/**
+	 * The mashup to observe. 
+	 */
+	private Mashup mashupToObserve;
+
+	/**
+	 * Reference to the create delete adapter.
+	 */
+	private Adapter createDeleteAdapter;
+
+	/**
+	 * Reference to the change adapter.
+	 */
+	private EContentAdapter changeAdapter;
+
+	/**
+	 * Creates a new observer that observes changes on the given mashup mashup and notifies the given objectToNotify.
+	 * 
+	 * @param mashup Mashup to observe.
+	 * @param objectToNotify Object to notify on changes
+	 */
+	public MashupChangeObserver(Mashup mashup, MashupChangedInterface objectToNotify) {
+		
+		this.observerToNotify = objectToNotify;
+		this.mashupToObserve = mashup;
+		
+		if (mashup != null && objectToNotify != null) {
+			
+			changeAdapter = new EContentAdapter() {
+				public void notifyChanged(Notification notification) {
+					// the configuration changed method on changes
+					observerToNotify.mashupConfigurationChanged(notification);
+					super.notifyChanged(notification);
+
+				}
+			};
+			// add adapter
+			mashup.eAdapters().add(changeAdapter);
+		}
+	}
+	
+	/**
+	 * Disconnects this observer from the mashup.
+	 */
+	public void disconnect()
+	{
+		if(mashupToObserve == null)
+		{
+			return;
+		}
+		
+		// remove adapters for the mashup
+		mashupToObserve.eAdapters().remove(createDeleteAdapter);
+		mashupToObserve.eAdapters().remove(changeAdapter);
+	}
+}
---
+++ util/ConfigurationObservers/src/org/sociotech/communitymashup/configuration/observer/mashup/MashupChangedInterface.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Peter Lachenmaier - Cooperation Systems Center Munich (CSCM).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Peter Lachenmaier - Design and initial implementation
+ ******************************************************************************/
+package org.sociotech.communitymashup.configuration.observer.mashup;
+
+import org.eclipse.emf.common.notify.Notification;
+
+/**
+ * Defining methods that will be called after the configuration changes
+ * 
+ * @author Peter Lachenmaier
+ */
+public interface MashupChangedInterface {
+
+	/**
+	 * This method will be call if the observed mashup gets changed
+	 * 
+	 * @param notification The EMF notification
+	 */
+	void mashupConfigurationChanged(Notification notification);
+
+}
