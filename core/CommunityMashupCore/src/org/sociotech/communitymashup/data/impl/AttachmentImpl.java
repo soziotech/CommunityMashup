ed29da6cc539459a3e26dfecc44a2ae28ba4d47d
Peter Lachenmaier <peter.lachenmaier@kooperationssysteme.de>
2013-04-30 17:24:33 +0200

    - fixed null pointer access when serializing duplicated attachments

--- core/CommunityMashupCore/src/org/sociotech/communitymashup/data/impl/AttachmentImpl.java
+++ core/CommunityMashupCore/src/org/sociotech/communitymashup/data/impl/AttachmentImpl.java
@@ -441,7 +441,7 @@ public abstract class AttachmentImpl extends ExtensionImpl implements Attachment
 	 * <!-- end-user-doc -->
 	 */
 	public String getCachedFileName() {
-		if(this.getDataSet().getCacheFileAttachements() == false)
+		if(this.getDataSet() != null && this.getDataSet().getCacheFileAttachements() == false)
 		{
 			// caching disabled
 			// reset cached file name
